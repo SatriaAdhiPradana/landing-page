439b6777025b520c43054ca3989006bad899a39d
Satria Adhi Pradana <92844616+SatriaAdhiPradana@users.noreply.github.com>
2023-02-25 11:52:59 +0700

    Create landingpage.php

---
+++ landingpage.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Landing Page</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a href="#features">Features</a></li>
+            <li><a href="#pricing">Pricing</a></li>
+            <li><a href="#about">About</a></li>
+        </ul>
+    </nav>
+    <header>
+        <h1>Welcome to My Landing Page</h1>
+        <p>Learn more about our product and why it's the best on the market.</p>
+        <a href="#features" class="cta">Get Started</a>
+    </header>
+    <section id="features">
+        <h2>Features</h2>
+        <ul>
+            <li>Feature 1</li>
+            <li>Feature 2</li>
+            <li>Feature 3</li>
+        </ul>
+    </section>
+    <section id="pricing">
+        <h2>Pricing</h2>
+        <p>Choose the plan that's right for you:</p>
+        <ul>
+            <li>Basic: $10/month</li>
+            <li>Pro: $20/month</li>
+            <li>Premium: $30/month</li>
+        </ul>
+    </section>
+    <section id="about">
+        <h2>About Us</h2>
+        <p>Learn more about our company and why we're the best at what we do.</p>
+        <a href="#" class="cta">Contact Us</a>
+    </section>
+    <footer>
+        <p>&copy; 2023 My Landing Page</p>
+    </footer>
+    <script src="script.js"></script>
+</body>
+</html>
